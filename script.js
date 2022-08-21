0ad9ce1a9eebc6ce3f017248ec6544209d0c46be
Meghanath Shetty <meghanathshetty108@gmail.com>
2022-08-21 22:58:13 +0530

    added save image button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#generate__form");
 const qrcode = document.querySelector("#qrcode");
 const screenWidth = window.innerWidth;
 const selectInput = document.querySelector("#size");
-
+const saveBtn = document.querySelector("#saveimage");
 
 
 if(screenWidth < 600){
@@ -18,9 +18,18 @@ const onSubmitGenerate = (e)=>{
     qrcode.innerHTML = "";
     const url = document.querySelector("#qrUrl").value;
     const size = document.querySelector("#size").value;
-    generateQRCode(url,screenWidth > 600 ? size: 200);
+    generateQRCode(url,(screenWidth > 600) ? size: 200);
     qrcode.style.display="flex";
- 
+
+    setTimeout(()=>{
+        const qrImgUrl = document.querySelector("#qrcode img").getAttribute('src');
+        console.log(qrImgUrl);
+        saveBtn.href = qrImgUrl;
+        saveBtn.style.display = "flex";
+        
+        
+    },1)
+   
 }
 
 const generateQRCode = (url,size)=>{
